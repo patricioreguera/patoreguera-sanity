05a08290c9ed47bf422d1c1f7cf203abb9c2a530
patricioreguera <reguerapatriciohernan@gmail.com>
2023-05-19 09:59:58 +0200

    env config

--- sanity.config.js
+++ sanity.config.js
@@ -3,8 +3,8 @@ import { deskTool } from "sanity/desk";
 import schemas from "./sanity/schemas";
 
 const config = defineConfig({
-	projectId: "2piqszv7",
-	dataset: "production",
+	projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
+	dataset: process.env.REACT_DATASET,
 	title: "patoreguera",
 	apiVersion: "2023-05-18",
 	basePath: "/admin",
